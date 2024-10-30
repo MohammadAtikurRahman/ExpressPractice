70bd8be1729ec1bcb536b7291a59034eb6f43176
atiktest2023 <contact.atikurrahman@gmail.com>
2024-10-30 15:56:34 +0600

    user creadted

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,24 +4,27 @@ const bcrypt = require('bcryptjs');
 // Controller to create a new user
 exports.createUser = async (req, res) => {
     try {
-        const { name, email, phone, password, userType } = req.body;
+        const { userName, userEmail, userPhone, userPassword, userType ,userId,userProfileId } = req.body;
 
         // Check if user already exists
-        const existingUser = await User.findOne({ email });
+        const existingUser = await User.findOne({ userEmail });
         if (existingUser) {
             return res.status(400).json({ message: 'User already exists' });
         }
 
-        // Hash the password
-        const hashedPassword = await bcrypt.hash(password, 10);
+        // Hash the userPassword
+        const hashedPassword = await bcrypt.hash(userPassword, 10);
 
         // Create a new user
         const newUser = new User({
-            name,
-            email,
-            phone,
-            password: hashedPassword,
-            userType
+            userId,
+            userType,
+            userProfileId,
+            userName,
+            userEmail,
+            userPhone,
+            userPassword: hashedPassword,
+          
         });
 
         await newUser.save();
--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    phone: { type: String, required: true },
-    password: { type: String, required: true },
+    userName: { type: String, required: true },
+    userEmail: { type: String, required: true, unique: true },
+    userPhone: { type: String, required: true },
+    userPassword: { type: String, required: true },
     userType: { type: String, required: true },
+    userId: { type: String, required: true },
+    userProfileId: { type: String, required: true },
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
+
